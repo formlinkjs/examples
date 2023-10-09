1842d39371b7e8e9a9d984870493a7b59a778fce
Jerome (Thavarshan) Thayananthajothy <jerome@netwatchglobal.com>
2023-10-09 03:19:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Formlink - Examples
+
+This repository contains examples of how to use the [Formlink](https://formlinkjs.com) library.
+
---
+++ vue3/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Form } from 'formlink';
+import { reactive } from 'vue';
+
+const form = reactive(new Form({
+    name: '',
+    username: '',
+    email: '',
+    photo: '',
+}));
+
+const updateUserProfile = () => {
+    form.put('/register', {
+        __method: 'PUT',
+        onSuccess: () => console.log('success'),
+        onError: () => console.log('error'),
+        onFinish: () => form.reset(),
+    });
+};
+</script>
+
+<template>
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+</template>
